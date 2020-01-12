0a30dd7b7a76296514441c71011e865e3f18da7f
superbe <babarykin@list.ru>
2020-01-12 22:08:44 +0500

    Дз к уроку 6

--- Lesson6/instalog/items.py
+++ Lesson6/instalog/items.py
@@ -6,9 +6,26 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import TakeFirst
 
 
 class InstalogItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    _id = scrapy.Field(output_processor=TakeFirst())
+    url = scrapy.Field(output_processor=TakeFirst())
+    parse_date = scrapy.Field(output_processor=TakeFirst())
+    insta_id = scrapy.Field(output_processor=TakeFirst())
+    username = scrapy.Field(output_processor=TakeFirst())
+    full_name = scrapy.Field(output_processor=TakeFirst())
+    profile_pic_url = scrapy.Field(output_processor=TakeFirst())
+
+
+class InstagramUserProfile(InstalogItem):
+    pass
+
+
+class InstagramUserFollowers(InstalogItem):
+    pass
+
+
+class InstagramUserFollowing(InstalogItem):
     pass
--- Lesson6/instalog/pipelines.py
+++ Lesson6/instalog/pipelines.py
@@ -4,8 +4,14 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from datetime import datetime
+from Lesson6.instalog.settings import mongo_client
 
 
 class InstalogPipeline(object):
     def process_item(self, item, spider):
+        data_base = mongo_client[spider.name]
+        collection = data_base[type(item).__name__]
+        item.update({'parse_date': datetime.now()})
+        collection.insert(item)
         return item
