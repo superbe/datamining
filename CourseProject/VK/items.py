02d17abc524589dea3aaf90cdd28be85c4977dc4
superbe <babarykin@list.ru>
2020-01-23 04:15:13 +0500

    Курсовой проект

---
+++ CourseProject/VK/items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, ChainMap
+
+
+def format_chain(value):
+    return f'https://vk.com/{value}'
+
+
+class VkItem(scrapy.Item):
+    _id = scrapy.Field(output_processor=TakeFirst())
+    url = scrapy.Field(output_processor=TakeFirst())
+    parse_date = scrapy.Field(output_processor=TakeFirst())
+    person_a = scrapy.Field(output_processor=TakeFirst())
+    person_b = scrapy.Field(output_processor=TakeFirst())
+    chain = scrapy.Field(input_processor=MapCompose(format_chain))
